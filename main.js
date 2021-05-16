961a01d598164de72800b924e8f466004f4edf0c
tilda2021 <82842860+tilda2021@users.noreply.github.com>
2021-05-16 00:00:54 -0600

    Week4

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ const quiz = [
 ];
 const game = {
     start(quiz){
-        this.questions = [...quiz];
         this.score = 0;
+        this.questions = [...quiz];
+        view.setup();
+        this.ask();
         
         //main game loop
         for(const question of this.questions){
@@ -26,6 +28,26 @@ const game = {
         result: document.getElementById('result'),
         info: document.getElementById('info'),
         start: document.getElementById('start'),
+        response: document.querySelector('#response'),
+        setup(){
+            this.show(this.question);
+            this.show(this.response);
+            this.show(this.result);
+            this.hide(this.start);
+            this.render(this.score,game.score);
+            this.render(this.result,'');
+            this.render(this.info,'');
+            this.resetForm();
+        }
+        teardown(){
+            this.hide(this.question);
+            this.hide(this.response);
+            this.show(this.start);
+        }
+        resetForm(){
+            this.response.answer.value = '';
+            this.response.answer.focus();
+        }
         show(element){
             element.style.display = 'block';
         },
@@ -39,10 +61,18 @@ const game = {
         target.innerHTML = content;
     }
 }
-    ask(){
+    ask(name){
+       // const question = `What is ${this.question.name}'s real name?`;
+       // const response = prompt(question);
+       // this.check(response);
+       if(this.questions.length > 0) {
+        this.question = this.questions.pop();
         const question = `What is ${this.question.name}'s real name?`;
-        const response = prompt(question);
-        this.check(response);
+        view.render(view.question,question);
+    }
+    else {
+        this.gameOver();
+    }
     },
 };
     check(response){
@@ -56,14 +86,47 @@ const game = {
                 alert(`Wrong! The correct answer was ${answer}`);
             }
         },
+       /* check(event){
+            event.preventDefault();
+            const response = view.response.answer.value;
+            const answer = this.question.realName;
+            if(response === answer){
+                view.render(view.result,'Correct!',{'class':'correct'});
+                this.score++;
+                view.render(view.score,this.score);
+            } else {
+                view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+            }
+            this.ask();
+        },
         gameOver(){
             view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
 
 
         }
+    }*/
+    check(event){
+        event.preventDefault();
+        const response = view.response.answer.value;
+        const answer = this.question.realName;
+        if(response === answer){
+            view.render(view.result,'Correct!',{'class':'correct'});
+            this.score++;
+            view.render(view.score,this.score);
+        } else {
+            view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+        }
+        gameOver(){
+            view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+            view.teardown();
+        }
+        view.resetForm();
+        this.ask();
     }
 }
 //Include the namespace in the function that starts the game
-game.start(quiz);
+//game.start(quiz);
 view.hide(view.start);
 view.start.addEventListener('click', () => game.start(quiz), false);
+view.response.addEventListener('submit', (event) => game.check(event), false);
+view.hide(view.response);
