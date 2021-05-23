cfd86eecfc23fa5323a6a0194fb14622d53d6cd1
tilda2021 <82842860+tilda2021@users.noreply.github.com>
2021-05-22 21:20:59 -0600

    Week05

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
 /* Chaper 5 & 6: Objects  */
 /* QUIZ NINJA PROJECT */
+/* WEEK 05
+Chapter 10: TESTING AND DEBUGGING
 
+Debugging is basically an error check. It is the process of finding out where buggs occur in our code and then fixing them. 
+Testing and Debugging are important to prevent failures in programs that may be difficult to spot and fix.
+To be a successful programmer, one must learn to master the act of testing and debugging
+There are many reasons why an error might occur in our code, a few are;
+    * System Error
+    * Programmer Error e.g incorrect syntax, and typo
+    * User Error
+     
+Exception: Exceptions are errors that return a value that can be used by the program to fix the error
+
+Stack Traces: These are sequence of functions or method calls that lead to the point where the error occurred.
+
+Warnings: Warnings occur if there is an error in the code that is not severe enough to cause the program to crash for example, a warning about an undeclared variable
+
+Strict Mode can be set when coding to improve clarity and speed. It throws exceptions when something is wrong with the code.
+To use the strict mode, add this string 'use strict'; to the first line of a JavaScript file.
+
+Linting Tool: Examples of linting tools are JS Lint, JS Hint, and ES Lint. These can be used to test the quality of JavaScript code.
+Passing a lint test is not a guarantee that a code is correct but it means the code is more consistent and unlikely to have problems.
+
+Feature Detection: This checks whether a method is supported before it is called, this helps to avoid an exception from being thrown
+
+The console and browser's have built-in debugging tool that can be used to interactively find and fix bugs in code
+
+Throw Statement: This can be applied to any JS expression to stop the execution of thhe program.
+
+Error Object: Eror objects can be created by the host environment when an exception occurs, or it can be created in the code using a constructor function in this manner,
+
+        "const error = new Error();"
+
+Any code placed inside a try block will pass any error objects to a catch block when an exception occurs. Any code inside a finally block will run if an exception does or does not occur.
+
+Test-driven development is the practice of writing tests that fail, then writing the code that passes the test, then refactoring the code every time a new feature is implemented.
+
+The Jest framework can be used to test your code.
+ */
 const quiz = [
     {name: "Superman", realName: "Clark Kent"},
     {name: "Wonder Woman", realName: "Diana Prince"},
@@ -8,6 +46,7 @@ const quiz = [
 ];
 const game = {
     start(quiz){
+        console.log('start() invoked');
         this.score = 0;
         this.questions = [...quiz];
         view.setup();
@@ -62,6 +101,7 @@ const game = {
     }
 }
     ask(name){
+        console.log('ask() invoked');
        // const question = `What is ${this.question.name}'s real name?`;
        // const response = prompt(question);
        // this.check(response);
@@ -106,6 +146,7 @@ const game = {
         }
     }*/
     check(event){
+        console.log('check(event) invoked');
         event.preventDefault();
         const response = view.response.answer.value;
         const answer = this.question.realName;
@@ -117,6 +158,7 @@ const game = {
             view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
         }
         gameOver(){
+            console.log('gameOver() invoked');
             view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
             view.teardown();
         }
